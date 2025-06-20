1fc9fc438fff560565d4c82ac48361f5330924a8
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-20 21:23:39 +0600

    watched 17-4: Explored more about schemas (defining data structure inside object like syntax)

--- log.js
+++ log.js
@@ -1,3 +1,4 @@
 // watched 17-1: understood about why monggose is used and got an intro to mongoose
 // watched 17-2: created basic server app and connected to mongodb database (mongoose)
 // watched 17-3: created a model and used it to create a document in the database
+// watched 17-4: Explored more about schemas (defining data structure inside object like syntax)
--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,36 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-const noteSchema = new Schema({ title: String, content: String });
+const noteSchema = new Schema({
+  title: { type: String, required: true, trim: true },
+  content: { type: String, default: "" },
+  category: {
+    type: String,
+    enum: ["personal", "work", "study", "other"],
+    default: "personal",
+  },
+  pinned: {
+    type: Boolean,
+    default: false,
+  },
+  tags: {
+    label: { type: String, required: true },
+    color: { type: String, default: "gray" },
+  },
+});
 // Define the Note model
 const Note = model("Note", noteSchema);
 
 app.post("/create-note", async (req: Request, res: Response) => {
   const myNote = new Note({
     title: "learning mongoose",
-    content: "I am learning mongoose",
+    tags: {
+      label: "mongoose",
+    },
   });
 
   await myNote.save();
-  
+
   res.status(201).json({
     success: true,
     message: "Note created successfully",
