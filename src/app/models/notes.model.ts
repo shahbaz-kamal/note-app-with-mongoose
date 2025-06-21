0c388de2f3ef756cc1c52ad98c58f7ba8f53d4e6
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-21 15:05:54 +0600

    watched 17-7: implemented MVC architecture in the app and created a controller to handle requests

---
+++ src/app/models/notes.model.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: "" },
+    category: {
+      type: String,
+      enum: ["personal", "work", "study", "other"],
+      default: "personal",
+    },
+    pinned: {
+      type: Boolean,
+      default: false,
+    },
+    tags: {
+      label: { type: String, required: true },
+      color: { type: String, default: "gray" },
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+// Define the Note model
+export const Note = model("Note", noteSchema);
+
